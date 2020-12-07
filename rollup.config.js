5f3ffaf3885782ce1aeee796d6364ee01bfa14d4
tastefinger <71318140+tastefinger@users.noreply.github.com>
2020-12-07 16:48:29 +0100

    setup project

---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+import babel from "@rollup/plugin-babel";
+import resolve from "@rollup/plugin-node-resolve";
+import pkg from "./package.json";
+
+const extensions = [".js", ".ts"];
+
+export default [
+	{
+		input: "src/index.ts",
+		output: [
+			{
+				file: pkg.main,
+				format: "cjs",
+			},
+			{
+				file: pkg.module,
+				format: "esm",
+			},
+		],
+		plugins: [
+			resolve({ extensions }),
+			babel({ extensions, babelHelpers: "bundled" }),
+		],
+	},
+];
